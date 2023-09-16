b88cc61229cba1a865a511959eebbdb0b31bd353
Pedro Henrique Silva <pedroupandofacil@hotmail.com>
2023-09-16 19:08:41 -0300

    Fazendo uma mini alteração

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ const btnSubmit = document.getElementById("submitButton");
 const results = document.getElementById("starsRating");
 const mainCard = document.querySelector(".mainCard");
 const thanksCard = document.querySelector(".thanksCard");
+const activeButton = document.querySelectorAll(".btn", ".active");
 let rating;
 
 const handleClick = (e) => {
+  activeButton.forEach((btn) => {
+    btn.classList.remove("active");
+  });
   const buttonTarget = e.target;
   rating = buttonTarget.value;
+  buttonTarget.classList.add("active");
 };
 
 const showValues = () => {
